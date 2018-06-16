2a70f5a0aa0f8647e8c9d5681cb508c757d3e81a
Vincent Rialland <vincent.rialland@gmail.com>
2018-06-16 18:26:19 +0200

    Fix main

--- laboate/main.py
+++ laboate/main.py
@@ -1,12 +1,12 @@
 from machine import I2C
 from machine import Pin
 
-import config
+from config import CONFIG
 from ssd1306 import SSD1306_I2C
 
 
 # Setup screen
-screen_config = config['ssd1306']
+screen_config = CONFIG['ssd1306']
 i2c = I2C(scl=Pin(screen_config['scl']),
           sda=Pin(screen_config['sda']))
 screen = SSD1306_I2C(screen_config['width'],
