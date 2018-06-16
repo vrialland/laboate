b906d5ecd080cdcc2f66d6d101e8fe243d4e7986
Vincent Rialland <vincent.rialland@gmail.com>
2018-06-16 21:44:17 +0200

    Add Wifi connection

--- laboate/main.py
+++ laboate/main.py
@@ -1,5 +1,5 @@
-from machine import I2C
-from machine import Pin
+from machine import idle, I2C, Pin
+import network
 
 from config import CONFIG
 from ssd1306 import SSD1306_I2C
@@ -13,6 +13,16 @@ screen = SSD1306_I2C(screen_config['width'],
                      screen_config['height'],
                      i2c,
                      addr=screen_config['address'])
+
+# Setup Wifi connection
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+print('Connecting to Wifi network "{}"'.format(CONFIG['wifi']['ssid']))
+sta_if.connect(CONFIG['wifi']['ssid'], CONFIG['wifi']['password'])
+while not sta_if.isconnected():
+    idle()
+    print('.', end='')
+print()
 
 # Test display
 screen.fill(0)
