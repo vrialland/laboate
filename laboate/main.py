01dd831772af50e7f376b37f20d698de4c6ddf08
Vincent Rialland <vincent.rialland@gmail.com>
2018-06-26 09:37:17 +0200

    Move screen and wifi setup outside loop

--- laboate/main.py
+++ laboate/main.py
@@ -9,22 +9,23 @@ import config
 import uasyncio as asyncio
 
 
+# Setup screen
+i2c = I2C(scl=Pin(config.SSD1306_SCL), sda=Pin(config.SSD1306_SDA))
+screen = SSD1306_I2C(config.DISPLAY_WIDTH, config.DISPLAY_HEIGHT, i2c,
+                     addr=config.SSD1306_ADDRESS)
+
+# Setup Wifi connection
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+print('Connecting to wifi network')
+sta_if.connect(config.WIFI_SSID, config.WIFI_PASSWORD)
+while not sta_if.isconnected():
+    idle()
+    print('.', end='')
+print()
+
+
 async def main_loop():
-    # Setup Wifi connection
-    sta_if = network.WLAN(network.STA_IF)
-    sta_if.active(True)
-    print('Connecting to wifi network')
-    sta_if.connect(config.WIFI_SSID, config.WIFI_PASSWORD)
-    while not sta_if.isconnected():
-        idle()
-        print('.', end='')
-    print()
-
-    # Setup screen
-    i2c = I2C(scl=Pin(config.SSD1306_SCL), sda=Pin(config.SSD1306_SDA))
-    screen = SSD1306_I2C(config.DISPLAY_WIDTH, config.DISPLAY_HEIGHT, i2c,
-                         addr=config.SSD1306_ADDRESS)
-
     # Setup lenuage
     nuage = LeNuage(config.LENUAGE_BASE_URL, config.LENUAGE_API_KEY)
 
