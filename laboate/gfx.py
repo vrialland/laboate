02c1664c4c92701fe66394d9367519238f004852
Vincent Rialland <vincent.rialland@gmail.com>
2018-06-26 23:52:19 +0200

    Add transitions

--- laboate/gfx.py
+++ laboate/gfx.py
@@ -1,5 +1,15 @@
 from font import FONT_4x6, FONT_5x7, FONT_4x6_CHARS, FONT_5x7_CHARS
 from utils import timeit
+import time
+from micropython import const
+
+
+# Scroll directions
+TRANSITION_LEFT = const(1)
+TRANSITION_RIGHT = const(2)
+TRANSITION_UP = const(3)
+TRANSITION_DOWN = const(4)
+_TRANSITION_STEPS = const(32)
 
 
 def render_string(fb, string, font=FONT_5x7, x=0, y=0, scale=1):
@@ -42,3 +52,40 @@ def render_tile_item(fb, item, scale=1):
         render_bitmap(fb, item['width'], item['height'], int(item['content']),
                       item['x'], item['y'], True, scale)
     return fb
+
+
+def scroll(screen, old_fb, new_fb, width, height, direction=TRANSITION_LEFT,
+           duration=500):
+    screen.fill(0)
+    # Show old framebuffer
+    screen.blit(old_fb, 0, 0)
+    screen.show()
+    # Clear screen
+    screen.fill(0)
+
+    sleep = duration // _TRANSITION_STEPS
+
+    if direction == TRANSITION_LEFT:
+        for x in range(0, width + 1, width // _TRANSITION_STEPS):
+            screen.blit(old_fb, -x, 0)
+            screen.blit(new_fb, width - x, 0)
+            screen.show()
+            time.sleep_ms(sleep)
+    elif direction == TRANSITION_RIGHT:
+        for x in range(0, width + 1, width // _TRANSITION_STEPS):
+            screen.blit(old_fb, x, 0)
+            screen.blit(new_fb, x - width, 0)
+            screen.show()
+            time.sleep_ms(sleep)
+    elif direction == TRANSITION_UP:
+        for y in range(0, height + 1, height // _TRANSITION_STEPS):
+            screen.blit(old_fb, 0, -y)
+            screen.blit(new_fb, 0, height - y)
+            screen.show()
+            time.sleep_ms(sleep)
+    elif direction == TRANSITION_DOWN:
+        for y in range(0, height + 1, height // _TRANSITION_STEPS):
+            screen.blit(old_fb, 0, y)
+            screen.blit(new_fb, 0, y - height)
+            screen.show()
+            time.sleep_ms(sleep)
--- laboate/main.py
+++ laboate/main.py
@@ -1,8 +1,9 @@
 from machine import idle, I2C, Pin
+import framebuf
 import network
 import time
 
-from gfx import render_tile_item
+from gfx import render_tile_item, scroll
 from lenuage import LeNuage
 from ssd1306 import SSD1306_I2C
 import config
@@ -13,6 +14,13 @@ import uasyncio as asyncio
 i2c = I2C(scl=Pin(config.SSD1306_SCL), sda=Pin(config.SSD1306_SDA))
 screen = SSD1306_I2C(config.DISPLAY_WIDTH, config.DISPLAY_HEIGHT, i2c,
                      addr=config.SSD1306_ADDRESS)
+# TODO Choose right color format
+buffer_a = framebuf.FrameBuffer(
+    bytearray(config.DISPLAY_WIDTH * config.DISPLAY_HEIGHT // 8),
+    config.DISPLAY_WIDTH, config.DISPLAY_HEIGHT, framebuf.MONO_VLSB)
+buffer_b = framebuf.FrameBuffer(
+    bytearray(config.DISPLAY_WIDTH * config.DISPLAY_HEIGHT // 8),
+    config.DISPLAY_WIDTH, config.DISPLAY_HEIGHT, framebuf.MONO_VLSB)
 
 # Setup Wifi connection
 sta_if = network.WLAN(network.STA_IF)
@@ -29,21 +37,31 @@ async def main_loop():
     # Setup lenuage
     nuage = LeNuage(config.LENUAGE_BASE_URL, config.LENUAGE_API_KEY)
 
+    # Display buffer a
+    screen.blit(buffer_a, 0, 0)
+    screen.show()
+
     while True:
         print('Fetching tiles informations')
         tiles_data = await nuage.get_tiles()
         for tile in tiles_data['tiles']:
             print('Fetching tile data')
             tile_data = await nuage.get_tile(tile['id'])
-            # Clear screens
+            # Clear screen and buffer b
             screen.fill(0)
-            # Render tiles
-            for item in tile_data[b'items']:
-                render_tile_item(screen, item, config.DISPLAY_SCALE)
-            # Update display
-            screen.show()
+            buffer_b.fill(0)
+            # Render tiles on buffer b
+            for item in tile_data['items']:
+                render_tile_item(buffer_b, item, config.DISPLAY_SCALE)
+            # Animate
+            scroll(screen, buffer_a, buffer_b,
+                   config.DISPLAY_WIDTH, config.DISPLAY_HEIGHT)
             # Wait
-            time.sleep_ms(tile_data[b'duration'])
+            time.sleep_ms(tile_data['duration'])
+
+            # Copy buffer b on a
+            buffer_a.fill(0)
+            buffer_a.blit(buffer_b, 0, 0)
     print('Done')
 
 # Setup asyncio
